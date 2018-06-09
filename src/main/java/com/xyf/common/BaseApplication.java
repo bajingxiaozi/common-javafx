a15157f8743fd6df42e0f4ea99217e4c4f152032
getting.xi <getting.xi@gmail.com>
2018-06-09 12:09:49 +0800

    整理代码

---
+++ src/main/java/com/xyf/common/BaseApplication.java
@@ -0,0 +1,28 @@
+package com.xyf.common;
+
+import com.xyf.common.util.Lg;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public abstract class BaseApplication extends Application {
+
+    private static final String TAG = BaseApplication.class.getSimpleName();
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        Lg.i(TAG, this);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        Lg.i(TAG, this);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Lg.i(TAG, this);
+    }
+
+}
